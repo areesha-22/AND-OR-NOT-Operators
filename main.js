3d06d089b73480c58e9b0df50c6a5c36836dce6d
areesha-22 <110978549+areesha-22@users.noreply.github.com>
2024-03-21 02:40:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// && opearotors
+var n = 'ali';
+var s = 'tuesday';
+console.log(n === 'ali' && s === "tuesday");
+// || operators
+var class1 = 12;
+var class3 = "areesha";
+console.log(class1 === 12 || class3 === "ali");
+// !opeartors
+var class0 = 34;
+var class10 = 32;
+console.log(!(class0 >= class10));
---
+++ main.ts
@@ -0,0 +1,15 @@
+
+// && opearotors
+let n='ali'
+let s='tuesday'
+console.log(n==='ali' && s==="tuesday")
+
+// || operators                             `
+let class1=12
+let class3="areesha"
+console.log(class1===12 || class3==="ali")
+
+// !opeartors
+let class0=34
+let class10=32
+console.log(!(class0>=class10))
